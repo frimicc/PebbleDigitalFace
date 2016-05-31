9b4fe528a31ab94d00738c60afb6361ffb0b1c2a
Michael Friedman <frimicc@users.noreply.github.com>
2016-05-31 00:07:57 -0700

    Add window handlers.

--- src/Main.c
+++ src/Main.c
@@ -3,11 +3,6 @@
 static Window *s_main_window;
 
 static void main_window_load(Window *window) {
-  s_main_window = window_create();
-  window_set_window_handlers(s_main_window, (WindowHandlers){
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
   
 }
 
@@ -16,11 +11,18 @@ static void main_window_unload(Window *window) {
 }
 
 static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers){
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
   
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
-  
+  window_destroy(s_main_window);
 }
 
 int main(void) {
